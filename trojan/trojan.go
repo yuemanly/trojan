a3fd2a9b9e766befac2d7d0cb37995ea4edbb825
yuemanly <yuemanly@163.com>
2024-12-31 22:00:46 +0800

    fix bugs

--- trojan/trojan.go
+++ trojan/trojan.go
@@ -8,6 +8,7 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"syscall"
 	"trojan/core"
 	"trojan/util"
 )
@@ -34,7 +35,7 @@ func ControlMenu() {
 	case 5:
 		go util.Log("trojan", 300)
 		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, os.Kill)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 		//阻塞
 		<-c
 	case 6:
